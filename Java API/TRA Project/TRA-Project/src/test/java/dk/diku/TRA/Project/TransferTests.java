ea7886562e392e5f8a664faf9c9ecda8c8212c86
Daniel Alstrup <dsal@netcompany.com>
2022-03-09 18:10:26 +0100

    Couple of tests for Transfer.validTransfer

--- Java API/TRA Project/TRA-Project/src/test/java/dk/diku/TRA/Project/TransferTests.java
+++ Java API/TRA Project/TRA-Project/src/test/java/dk/diku/TRA/Project/TransferTests.java
@@ -1,2 +1,52 @@
-package dk.diku.TRA.Project;public class TransferTests {
+package dk.diku.TRA.Project;
+
+import Classes.Agent;
+import Classes.Resource;
+import Classes.Transfer;
+import Exceptions.TRAException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TransferTests {
+
+    ////////////////////////////////////////////////////////////////
+    /////////////////////// Valid tests ////////////////////////////
+    ////////////////////////////////////////////////////////////////
+    @Test  ///////////////// This might need extension if we decide that valid transfers should also check if the parties can afford the transfer.
+    void TestZeroSum() {
+        try{
+            Resource a1 = new Resource("a", 42);
+            Resource a2 = new Resource("a", -42);
+            Resource b1 = new Resource("b", 1337);
+            Resource b2 = new Resource("b", -1337);
+            a1.add(b2);
+            a2.add(b1);
+            Agent alice = new Agent("Alice");
+            Agent bob = new Agent("Bob");
+            Transfer t = new Transfer(new ArrayList<>() {{add(alice); add(bob);}},
+                                        new ArrayList<>(){{add(a1); add(a2);}});
+            assert(t.validTransfer());
+        } catch (TRAException e){
+            assert(true);
+        }
+    }
+    @Test  ///////////////// This might need extension if we decide that valid transfers should also check if the parties can afford the transfer.
+    void TestZeroSumFail() {
+        try{
+            Resource a1 = new Resource("a", 41);
+            Resource a2 = new Resource("a", -42);
+            Resource b1 = new Resource("b", 1338);
+            Resource b2 = new Resource("b", -1337);
+            a1.add(b2);
+            a2.add(b1);
+            Agent alice = new Agent("Alice");
+            Agent bob = new Agent("Bob");
+            Transfer t = new Transfer(new ArrayList<>() {{add(alice); add(bob);}},
+                    new ArrayList<>(){{add(a1); add(a2);}});
+            assert(!t.validTransfer());
+        } catch (TRAException e){
+            assert(true);
+        }
+    }
 }
