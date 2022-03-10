be8dc5881619fd5158f0dd29eca060c4fbed966e
Alexander <cqv636@alumni.ku.dk>
2022-03-10 18:31:47 +0100

    Added minor attributes to agents and events

--- Java API/TRA Project/TRA-Project/src/main/java/Classes/Agent.java
+++ Java API/TRA Project/TRA-Project/src/main/java/Classes/Agent.java
@@ -4,18 +4,30 @@ import Exceptions.ExceptionConstants;
 import Exceptions.TRAException;
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.UUID;
+
 public class Agent {
 
     public String name;
+    private final UUID uuid;
 
     public Agent(String name) throws TRAException {
         if (name == null){
             throw new TRAException(ExceptionConstants.GENERIC_ERROR);
         }
         this.name = name;
+        this.uuid = UUID.randomUUID();
     }
 
-    @Override
+    public String getName() {
+        return name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    // @Override
     public void sign(Event event) {
     }
 }
--- Java API/TRA Project/TRA-Project/src/main/java/Classes/Event.java
+++ Java API/TRA Project/TRA-Project/src/main/java/Classes/Event.java
@@ -2,14 +2,23 @@ package Classes;
 
 import Interfaces.IEvent;
 import java.text.SimpleDateFormat;
+import java.util.List;
+
 public class Event implements IEvent {
 
-    private Agent responsible;
+    public String eventName;
+    private List<Agent> responsible;
     private SimpleDateFormat time;
     private Information body;
 
+    public Event(String eventName, List<Agent> responsible, Information body) {
+        this.eventName = eventName;
+        this.responsible = responsible;
+        this.time = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");;
+        this.body = body;
+    }
 
-    public Agent getResponsible() {
+    public List<Agent> getResponsible() {
         return responsible;
     }
     public SimpleDateFormat getTime() {
@@ -17,11 +26,5 @@ public class Event implements IEvent {
     }
     public Information getBody() {
         return body;
-    }
-    
-    public Event(Agent responsible, Information body) {
-        this.responsible = responsible;
-        this.time = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");;
-        this.body = body;
     }
 }
