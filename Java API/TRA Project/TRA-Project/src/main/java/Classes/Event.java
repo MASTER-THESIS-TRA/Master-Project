68267c6cb7a37edddedeab145ed3fd3978edbd31
Alexander <cqv636@alumni.ku.dk>
2022-03-10 17:23:06 +0100

    Minor change to event - need to pull :P

--- Java API/TRA Project/TRA-Project/src/main/java/Classes/Event.java
+++ Java API/TRA Project/TRA-Project/src/main/java/Classes/Event.java
@@ -2,12 +2,29 @@ package Classes;
 
 import Interfaces.IEvent;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Event implements IEvent {
 
-    public Agent responsible;
-    public Date time;
-    public Information body;
+    private Agent responsible;
+    private SimpleDateFormat time;
+    private Information body;
 
+    public Agent getResponsible() {
+        return responsible;
+    }
+    public SimpleDateFormat getTime() {
+        return time;
+    }
+    public Information getBody() {
+        return body;
+    }
+
+    public Event(Agent responsible, Information body) {
+        this.responsible = responsible;
+        this.time = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");;
+        this.body = body;
+    }
 }
