d9b6afe27ecddccd744b9334d2e30a0cf80c8b7d
Daniel Alstrup <dsal@netcompany.com>
2022-03-15 17:29:23 +0100

    Initial commit on Daniel branch

--- Java API/TRA Project/TRA-Project/src/main/java/Interfaces/IVector.java
+++ Java API/TRA Project/TRA-Project/src/main/java/Interfaces/IVector.java
@@ -2,12 +2,13 @@ package Interfaces;
 
 import Classes.Vector;
 
+import java.util.AbstractMap;
 import java.util.Map;
 
 /**
  * This is a stateless interface. Implementations of this interface should adhere to the statelesness.
  */
-public interface IVector<K,V> extends Map<K,V> {
+public interface IVector<K,V> {
 
     public IVector zero();
     public IVector add(IVector x, IVector y);
