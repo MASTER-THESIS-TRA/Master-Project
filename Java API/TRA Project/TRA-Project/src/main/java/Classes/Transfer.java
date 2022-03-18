071a9bcce7396bcb6c307a762f71a246012d447f
Daniel Alstrup <dsal@netcompany.com>
2022-03-18 08:42:45 +0100

    Added equals method to Transfer

--- Java API/TRA Project/TRA-Project/src/main/java/Classes/Transfer.java
+++ Java API/TRA Project/TRA-Project/src/main/java/Classes/Transfer.java
@@ -70,4 +70,31 @@ public class Transfer extends Vector {
         }
         return null;
     }
+
+    @Override
+    public boolean equals(Object o){
+        try {
+            Transfer cmp = (Transfer) o;
+            // Creating temporary HashMaps to be able to modify them.
+            HashMap<Agent,Resource> tmpA = new HashMap(cmp);
+            HashMap<Agent,Resource> tmpB = new HashMap(this);
+            // Removing any keys that are zero, since any transfer implicitly transfers 0 resources to all agents not mentioned in the transfer.
+            tmpA.entrySet().removeIf(entry -> entry.getValue().equals(Resource.zero()));
+            tmpB.entrySet().removeIf(entry -> entry.getValue().equals(Resource.zero()));
+
+            // Verify that the remaining key are the same in both maps, then compare values.
+            if (tmpB.keySet().equals(tmpA.keySet())){
+                for (Agent k : tmpA.keySet()){
+                    if (!tmpB.get(k).equals(tmpA.get(k))){
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        } catch (ClassCastException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
