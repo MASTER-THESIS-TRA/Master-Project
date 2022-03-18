2c7dd6ab8ac62c2fb36a8f90adc0821b09fa8ee6
Daniel Alstrup <dsal@netcompany.com>
2022-03-18 08:38:48 +0100

    Added equals method to Resources

--- Java API/TRA Project/TRA-Project/src/main/java/Classes/ResourceManager.java
+++ Java API/TRA Project/TRA-Project/src/main/java/Classes/ResourceManager.java
@@ -28,44 +28,6 @@ public class ResourceManager extends Agent{
         balance.put(this,Resource.mult(initialBalance,-1));
         ownerships = Transfer.add(ownerships, new Transfer(balance));
     }
-
-    /*
-    public HashMap<Agent,Resource> ownerships;
-
-    public ResourceManager(){
-        ownerships = new HashMap<>();
-    }
-
-    public void addAgent (Agent a) throws TRAException {
-        if (a == null){
-            throw new TRAException(ExceptionConstants.GENERIC_ERROR + " agent " + a + " is null.");
-        }
-        if (ownerships.containsKey(a)){
-            throw new TRAException(ExceptionConstants.GENERIC_ERROR + " agent " + a + " already exists.");
-        }
-        ownerships.put(a, new Resource()); // When adding a new agent, do so with a fresh account (i.e. an empty resource).
-    }
-
-    public boolean verifyInternalTransfer(Transfer t){ // More complicated with other resource managers.
-        for (Agent a : t.transfers.keySet()){
-            if (!ownerships.get(a).canAdd(t.transfers.get(a))){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void completeInternalTransfer(Transfer t) throws TRAException {
-        if (!verifyInternalTransfer(t) || !(t.validTransfer())){
-            throw new TRAException(ExceptionConstants.GENERIC_ERROR);
-        }
-        else{
-            for (Agent a : t.transfers.keySet()){
-                ownerships.get(a).add(t.transfers.get(a));
-            }
-        }
-    }
-*/
 }
 
 
