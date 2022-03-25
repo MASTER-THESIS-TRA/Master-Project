ce7e65a971dcedb6e0d5c5208bfa6f6f484418e0
Daniel Alstrup <dsal@netcompany.com>
2022-03-25 17:35:51 +0100

    TransferTests rewritten

--- Java API/TRA Project/TRA-Project/src/test/java/dk/diku/TRA/Project/TransferTests.java
+++ Java API/TRA Project/TRA-Project/src/test/java/dk/diku/TRA/Project/TransferTests.java
@@ -47,10 +47,10 @@ public class TransferTests {
 
     @Test
     void TestAddCummutative(){  // a+b=b+a
-        Transfer t3 = Transfer.add(t1,t2);
-        Transfer t4 = Transfer.add(t2,t1);
-        assert(t3.equals(t4));
-        assert(t4.equals(t3));
+        Transfer t4 = Transfer.add(t1,t2);
+        Transfer t5 = Transfer.add(t2,t1);
+        assert(t4.equals(t5));
+        assert(t5.equals(t4));
     }
     @Test
     void TestAddAsociative(){ // (a+b)+c=a+(b+c)
@@ -70,76 +70,43 @@ public class TransferTests {
         assert(Transfer.zero().equals(Transfer.add(Transfer.mult(t1,-1),t1)));
     }
 
-
-
-
-
-
-
-
-
-
-
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
     ////////////////////////////////////////////////////////////////
     //////////////////////// Mult tests ////////////////////////////
     ////////////////////////////////////////////////////////////////
     @Test
-    void TestMultBasic(){
+    void TestMultBasic(){ // 1337*42 = 56154
         Integer b = 1337;
-        Transfer t2 = Transfer.mult(t1,b);
-        assert(t3.equals(t2));
-        assert(t2.equals(t3));
+        assert(Transfer.mult(t1,b).get("Alice").get("a")==56154);
     }
 
     @Test
-    void TestMultCummutative(){
-        assert(true); // Not sure if cummutative multiplication is really relevant to us.
-    }
-
-    @Test
-    void TestMultAssociative(){
-        Resource a = new Resource("a", 42);
-        Agent alice = new Agent("Alice");
+    void TestMultAssociative(){ // a*(b*c)=(a*b)*c variation 1
         Integer b = 1337;
         Integer c = 42;
-        Transfer t1 = new Transfer(alice,a);
-        Transfer t2 = Transfer.mult(Transfer.mult(t1,b),c);
-        Transfer t3 = Transfer.mult(Transfer.mult(t1,c),b);
-        assert(t3.equals(t2));
-        assert(t2.equals(t3));
+        Transfer t4 = Transfer.mult(Transfer.mult(t1,b),c);
+        Transfer t5 = Transfer.mult(Transfer.mult(t1,c),b);
+        assert(t4.equals(t5));
     }
 
     @Test
-    void TestMultAssociative2(){
-        Resource a = new Resource("a", 42);
-        Agent alice = new Agent("Alice");
+    void TestMultAssociative2(){ // a*(b*c)=(a*b)*c variation 2
         Integer b = 1337;
         Integer c = 42;
-        Transfer t1 = new Transfer(alice,a);
-        Transfer t2 = Transfer.mult(t1,b*c);
-        Transfer t3 = Transfer.mult(Transfer.mult(t1,b),c);
-        assert(t3.equals(t2));
-        assert(t2.equals(t3));
+        Transfer t4 = Transfer.mult(t1,b*c);
+        Transfer t5 = Transfer.mult(Transfer.mult(t1,b),c);
+        assert(t4.equals(t5));
     }
 
     @Test
-    void TestMultIdentity(){ // A bit of a special test case, as this is not actually the multiplicative identity of the vector space (one is not necessary).
-        Resource a = new Resource("a", 42);
-        Agent alice = new Agent("Alice");
-        Transfer t = new Transfer(alice, a);
-        assert(t.equals(Transfer.mult(t,1)));
+    void TestMultIdentity(){ //  t1*1=t1
+        // A bit of a special test case, as this is not actually the multiplicative identity of the vector space (one is not necessary).
+        assert(t1.equals(Transfer.mult(t1,1)));
     }
 
     @Test
-    void TestMultInverse(){ // Not multiplicativ inverse, but same reasoning as the testcase above stands to reason her.
-        Resource a = new Resource("a", 42);
-        Agent alice = new Agent("Alice");
-        Transfer t = new Transfer(alice, a);
-        assert(Transfer.zero().equals(Transfer.mult(t,0)));
+    void TestMultInverse(){ // t1*0=0
+        // Not multiplicativ inverse, but same reasoning as the testcase above stands to reason her.
+        assert(Transfer.zero().equals(Transfer.mult(t1,0)));
     }
 
     ////////////////////////////////////////////////////////////////
@@ -147,27 +114,18 @@ public class TransferTests {
     ////////////////////////////////////////////////////////////////
     @Test
     void TestDistributive1(){ // c * (t1 + t2) = (c * t1) + (c * t2)
-        Resource a = new Resource("a", 42);
-        Resource b = new Resource("b", 1337);
-        Agent alice = new Agent("Alice");
-        Agent bob = new Agent("Bob");
-        Transfer t1 = new Transfer(alice,Resource.add(a,Resource.mult(b,-1)));
-        Transfer t2 = new Transfer(bob,Resource.add(b,Resource.mult(a,-1)));
         Integer c = 888;
-        Transfer t3 = Transfer.mult(Transfer.add(t1,t2),c);
-        Transfer t4 = Transfer.add(Transfer.mult(t1,c),Transfer.mult(t2,c));
-        assert(t3.equals(t4));
+        Transfer t4 = Transfer.mult(Transfer.add(t1,t2),c);
+        Transfer t5 = Transfer.add(Transfer.mult(t1,c),Transfer.mult(t2,c));
+        assert(t5.equals(t4));
     }
     @Test
     void TestDistributive2(){ // (a + b) * t1 = (a * t1) + (b * t1)
         Integer a = 888;
         Integer b = 1337;
-        Resource c = new Resource("a", 42);
-        Agent alice = new Agent("Alice");
-        Transfer t1 = new Transfer(alice,c);
-        Transfer t2 = Transfer.mult(t1,(a+b));
-        Transfer t3 = Transfer.add(Transfer.mult(t1,a),Transfer.mult(t1,b));
-        assert(t3.equals(t2));
+        Transfer t4 = Transfer.mult(t1,(a+b));
+        Transfer t5 = Transfer.add(Transfer.mult(t1,a),Transfer.mult(t1,b));
+        assert(t4.equals(t5));
     }
 
     ////////////////////////////////////////////////////////////////
@@ -176,18 +134,7 @@ public class TransferTests {
     @Test
     ///////////////// This might need extension if we decide that valid transfers should also check if the parties can afford the transfer.
     void TestZeroSum() {
-        Resource a1 = new Resource("a", 42);
-        Resource a2 = new Resource("a", -42);
-        Resource b1 = new Resource("b", 1337);
-        Resource b2 = new Resource("b", -1337);
-        Resource finalA1 = Resource.add(a1,b2);
-        Resource finalA2 = Resource.add(a2,b1);
-        Agent alice = new Agent("Alice");
-        Agent bob = new Agent("Bob");
-        HashMap<Agent, Resource> M = new HashMap<>();
-        M.put(alice,finalA1);
-        M.put(bob,finalA2);
-        Transfer t = new Transfer(M);
+        Transfer t = Transfer.add(Transfer.add(t1,t2),t3);
         assert(t.Valid());
     }
     @Test  ///////////////// This might need extension if we decide that valid transfers should also check if the parties can afford the transfer.
@@ -203,7 +150,11 @@ public class TransferTests {
         HashMap<Agent, Resource> M = new HashMap<>();
         M.put(alice,finalA1);
         M.put(bob,finalA2);
-        Transfer t = new Transfer(M);
-        assert(!t.Valid());
+        try{
+            Transfer t = new Transfer(M);
+            assert(false);
+        } catch (TRAException e){
+            assert(true);
+        }
     }
 }
