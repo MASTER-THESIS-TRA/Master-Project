07e7a6eba21af1939a27f2daa3b83e7e13f2b338
Daniel Alstrup <dsal@netcompany.com>
2022-03-30 16:24:41 +0200

    Refactor folder structure

---
+++ Java API/TRA Project/TRA-Project/src/main/java/dk/diku/TRA/Project/controller/LoginController.java
@@ -0,0 +1,14 @@
+package dk.diku.TRA.Project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping(path = "/login")
+public class LoginController {
+    @GetMapping("/")
+    public String index (){
+        return "Welcome!";
+    }
+}
