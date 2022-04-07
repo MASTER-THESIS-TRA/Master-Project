a4d2cd33c08f8f7959649f5a42c94fbc383adf42
Daniel Alstrup <dsal@netcompany.com>
2022-04-07 17:39:29 +0200

    ResourceManager logic for weights and transformations

--- Java API/TRA Project/TRA-Project/src/main/java/Classes/ResourceManager.java
+++ Java API/TRA Project/TRA-Project/src/main/java/Classes/ResourceManager.java
@@ -1,6 +1,7 @@
 package Classes;
 
 import Exceptions.TRAException;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.scheduling.annotation.Async;
 
@@ -10,6 +11,7 @@ import java.util.Map;
 public class ResourceManager extends Agent{
     private Transfer ownerships; // Ownership state can be represented as a transfer, where the ResourceManager transfers everyone their resources.
     private CreditPolicy CP;
+    private Weight weights;
 
     public ResourceManager(String name){
         super(name);
@@ -26,12 +28,16 @@ public class ResourceManager extends Agent{
         CP = new CreditPolicy(this,new Resource("*",1));
         try { ownerships = new Transfer(M); } catch (TRAException e){ }
     }
-
-    // What Fritz??
-    public ResourceManager(ResourceManager a, ResourceManager b){
-        super(a.getName() + " and " + b.getName());
-        throw new NotImplementedException();
-        // And then add the ownerships of the two ResourceManagers together.
+    public ResourceManager(String name, Map<Agent,Resource> M, Map<String, Double> W){
+        super(name);
+        Resource debt = Resource.zero();
+        for (Resource r : M.values()){
+            debt = Resource.add(debt, Resource.mult(r,-1));
+        }
+        M.put(this,debt);
+        CP = new CreditPolicy(this,new Resource("*",1));
+        weights = new Weight(W);
+        try { ownerships = new Transfer(M); } catch (TRAException e){ }
     }
 
     // Initially an agent has no
@@ -55,7 +61,7 @@ public class ResourceManager extends Agent{
         return true;
     }
 
-    public boolean Apply(Transfer t){
+    public boolean ApplyTransfer(Transfer t){
         if (CP.ValidateTransfer(t, ownerships)){
             ownerships = Transfer.add(ownerships, t);
             return true;
@@ -63,9 +69,40 @@ public class ResourceManager extends Agent{
         return false;
     }
 
-    @Async
-    public boolean TwoPhaseCommit(Transfer t){
-        throw new NotImplementedException();
+    public boolean ApplyTransform(Transformation t){
+        if (checkWeightSum(t)){
+            ownerships = Transfer.add(ownerships, transformToTransfer(t));
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkWeightSum(Transformation t){
+        Double sum = 0.0;
+        for (Resource r : t.values()){
+            for (Map.Entry<String, Integer> e : r.entrySet()){
+                sum += weights.get(e.getKey())*e.getValue();
+            }
+        }
+        return sum == 0.0;
+    }
+
+    // This method handles the logic behind a manager "owing" the customers their resources.
+    // In essence the manager is paying back the consumed resources and now owes the produces resources.
+    private Transfer transformToTransfer(Transformation t){
+        Resource toManager = Resource.zero();
+        for (Resource r : t.values()){
+            toManager = Resource.add(toManager,Resource.mult(r,-1));
+        }
+        HashMap M = new HashMap<>(t);
+        M.put(this,toManager);
+        try{
+            return new Transfer(M);
+        } catch (TRAException e){
+            // This will not happen, as we are ensuring above, that the sum is always 0.
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     public Transfer GetOwnerships() {
@@ -78,3 +115,16 @@ public class ResourceManager extends Agent{
 }
 
 
+
+
+    /*// What Fritz??
+    public ResourceManager(ResourceManager a, ResourceManager b){
+        super(a.getName() + " and " + b.getName());
+        throw new NotImplementedException();
+        // And then add the ownerships of the two ResourceManagers together.
+    }
+
+    @Async
+    public boolean TwoPhaseCommit(Transfer t){
+        throw new NotImplementedException();
+    }*/
