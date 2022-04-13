3320833bb0f2c535aa88462629a9c66b01eb81c9
Alexander <cqv636@alumni.ku.dk>
2022-04-13 17:13:50 +0200

    Added changes to transfer component

--- Java API/TRA Project/TRA-Project/react-web-app/src/components/Transfer/ExchangeLogo.js
+++ Java API/TRA Project/TRA-Project/react-web-app/src/components/Transfer/ExchangeLogo.js
@@ -1,9 +1,21 @@
 import logo from '../../Resources/swap.png'
+import Container from "@mui/material/Container";
+
+const imgStyle = {
+    height: '100%',
+    display: 'block',
+    margin: 'auto',
+    maxWidth: '100%'
+}
+
+const divStyle = {
+    overflow: 'hidden'
+}
 
 export const ExchangeLogo = () => {
     return (
-        <div>
-            <img src={logo}/>
+        <div style={divStyle}>
+            <img style={imgStyle} src={logo}/>
         </div>
     )
 }
--- Java API/TRA Project/TRA-Project/react-web-app/src/pages/TransferPage.js
+++ Java API/TRA Project/TRA-Project/react-web-app/src/pages/TransferPage.js
@@ -1,22 +1,33 @@
-import {AgentBox} from "../components/Transfer/AgentBox";
+
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import {Paper} from "@mui/material";
 import {ResourceList} from "../components/Transfer/ResourceList";
-import {CustomButton} from "../components/customButton";
-import {ExchangeLogo} from "../components/Transfer/ExchangeLogo";
+import Title from "../components/Title";
+import React from "react";
+import {Transfer} from "../components/Transfer/Transfer";
+
 
 export const TransferPage = () => {
     return(
         <div>
-           <div>
-               <AgentBox agent={"Agent 1"}/>
-           </div>
-            <ResourceList listObj1={listObj1}/>
-            <CustomButton resourceList=listObj1/>
-            <ExchangeLogo/>
-            <div>
-                <AgentBox agent={"Agent 2"}/>
-            </div>
-            <ResourceList listObj2={listObj2}/>
-            <CustomButton resourceList = listObj2/>
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 600
+                        }}>
+                            <Title>Transfer Component</Title>
+                            {/* Render Transfer component */}
+                            <Transfer />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
