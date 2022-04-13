a365af36bfae0de852b812450f34fc2fd7d0c5fd
Alexander <cqv636@alumni.ku.dk>
2022-04-13 15:37:19 +0200

    Added skeleton to overview page

--- Java API/TRA Project/TRA-Project/react-web-app/src/pages/OverviewPage.js
+++ Java API/TRA Project/TRA-Project/react-web-app/src/pages/OverviewPage.js
@@ -1,9 +1,46 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import {Paper} from "@mui/material";
+
 export const OverviewPage = () => {
     return (
         <div>
-            <p>
-                Overview page
-            </p>
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                <Grid container spacing={3}>
+                    {/* Chart */}
+                    <Grid item xs={12} md={8} lg={7}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 240,
+                            }}
+                        >
+
+                        </Paper>
+                    </Grid>
+                    {/* Recent Deposits */}
+                    <Grid item xs={12} md={8} lg={5}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 240,
+                            }}
+                        >
+
+                        </Paper>
+                    </Grid>
+                    {/* Recent Orders */}
+                    <Grid item xs={12}>
+                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
