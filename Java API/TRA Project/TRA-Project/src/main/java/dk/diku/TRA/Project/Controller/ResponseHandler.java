7e0809148edb6f49ab61c7330e9c3d4d0b3fc41d
Daniel Alstrup <dsal@netcompany.com>
2022-04-19 19:58:10 +0200

    Helper class for constructing http responses

---
+++ Java API/TRA Project/TRA-Project/src/main/java/dk/diku/TRA/Project/Controller/ResponseHandler.java
@@ -0,0 +1,25 @@
+package dk.diku.TRA.Project.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class ResponseHandler {
+    /**
+     * @param message       The response message
+     * @param status        Http status of the response
+     * @param responseObj   The body of the response
+     * @return              Returns a spring ResponseEntity<Obj>
+     */
+    public static ResponseEntity generateResponse(String message, HttpStatus status, Object responseObj) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", message);
+        map.put("status", status.value());
+        map.put("data", responseObj);
+
+        return new ResponseEntity(map,status);
+    }
+}
