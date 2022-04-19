0a23afd3d56c60cc7303a2483943cda31097e7d4
Daniel Alstrup <dsal@netcompany.com>
2022-04-19 19:57:00 +0200

    Rest controller for overview page

--- Java API/TRA Project/TRA-Project/react-web-app/src/components/Overview/PendingTransactions.js
+++ Java API/TRA Project/TRA-Project/react-web-app/src/components/Overview/PendingTransactions.js
@@ -1,12 +1,7 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import {CustomTable} from "../CustomTable";
+
+const axios = require('axios');
 
 function createData(
     name,
@@ -18,13 +13,18 @@ function createData(
     return [ name, calories, fat, carbs, protein ];
 }
 
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const rows = () => {
+    console.log("start")
+     axios.get('http://localhost:8080/pendingTransactions')
+        .then((response) => {
+            console.log("response:");
+            console.log(response.data);
+        })
+         .catch((error) => {
+             console.log("error:");
+             console.log(error)
+         });
+}
 
 const cols = [
     'Name', 'Calories', 'Fat', 'Carbs', 'protein'
@@ -34,7 +34,7 @@ const cols = [
 export const PendingTransactions = () => {
     return (
         <div>
-            <CustomTable rows={rows} cols={cols}/>
+            <button onClick={rows()}>Fetch data</button>
         </div>
     )
 }
--- Java API/TRA Project/TRA-Project/react-web-app/src/components/Overview/RecentTransactions.js
+++ Java API/TRA Project/TRA-Project/react-web-app/src/components/Overview/RecentTransactions.js
@@ -1,8 +1,20 @@
+import {CustomTable} from "../CustomTable";
+
+const cols = [
+    'Type', 'Date', 'Net'
+];
+
+const rows = [
+    ['Transfer', '13/04-2022', '-40 Coffee Bag (500g), +200$'],
+    ['Transform', '03/04-2022', '-20 Roasted Coffee (kg), -40 Paper Bag (pcs), +40 Coffee Bag (500g)'],
+    ['Transfer', '01/04-2022', '-10$, +40 Paper Bag (pcs)'],
+    ['Transfer', '01/04-2022', '-100$, +20 Roasted Coffee (kg)']
+];
+
 export const RecentTransactions = () => {
     return (
         <div>
-            <p>Recent Transactions:<br/>
-                1) You transferred everything to charity, good on ya'!</p>
+            <CustomTable rows={rows} cols={cols}/>
         </div>
     )
 }
