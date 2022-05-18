7d52c605ddf884ee8cc54ba7ead6eac1f06ebad7
Daniel Alstrup <dsal@netcompany.com>
2022-05-18 15:15:11 +0200

    Email format verification added

--- Java API/TRA Project/TRA-Project/src/main/java/dk/diku/TRA/Project/Services/AgentService.java
+++ Java API/TRA Project/TRA-Project/src/main/java/dk/diku/TRA/Project/Services/AgentService.java
@@ -2,6 +2,7 @@ package dk.diku.TRA.Project.Services;
 
 
 import dk.diku.TRA.Project.Classes.Agent;
+import dk.diku.TRA.Project.Classes.Utils;
 import dk.diku.TRA.Project.repository.AgentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,16 @@ public class AgentService {
 
     @Autowired
     AgentRepository agentRepository;
+    Utils utils = new Utils();
 
     public Agent GetAgentById(String id){
         return agentRepository.findById(id).get();
     }
 
     public String SaveAgent (String name, String email, String password){
+        if (!utils.VerifyEmail(email)){
+            return "error";
+        }
         Agent agent = new Agent();
         agent.setName(name);
         agent.setEmail(email);
