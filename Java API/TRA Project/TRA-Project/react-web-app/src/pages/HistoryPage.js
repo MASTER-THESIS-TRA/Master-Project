7fb31d4f29e7d736ed566ff996161dd552c9956e
AlexBorgert <cqv636@alumni.ku.dk>
2022-05-20 00:45:06 +0200

    Changed history component - it now correctly displays history of events

--- Java API/TRA Project/TRA-Project/react-web-app/src/pages/HistoryPage.js
+++ Java API/TRA Project/TRA-Project/react-web-app/src/pages/HistoryPage.js
@@ -3,14 +3,44 @@ import {Paper} from "@mui/material";
 import {HistoryTable} from "../components/History/HistoryTable";
 import Title from "../components/Title";
 import * as React from "react";
+import {CustomTable} from "../components/CustomTable";
+import axios from "axios";
+import {useState, useEffect} from "react";
+
+const columns = [
+    { id: 'id', label: 'Id', minWidth: 100 },
+    { id: 'eventType', label: 'Event Type', minWidth: 100 },
+    { id: 'agentId', label: 'Created By', minWidth: 100 },
+    { id: 'time', label: 'Time Of Creation', minWidth: 100 },
+    { id: 'body', label: 'Name', minWidth: 200 },
+
+];
+
 
 export const HistoryPage = () => {
+    const[rows, setRows] = useState([]);
+
+    useEffect(async () => {
+        const data = [];
+        const res = await axios.get("http://localhost:8080/history/getAllEvents")
+        if(res.status != 500) {
+            res.data.map((elem) => {
+                data.push(createData(elem.id, elem.eventType, elem.agentId, elem.time, elem.body))
+            })
+            setRows(data);
+        }
+    }, [])
+
+    function createData(id, eventType, agentId, time, body) {
+        return { id, eventType, agentId, time, body };
+    }
+
     return(
         <div>
             <Grid item xs={12}>
                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', maxHeight:700, height: 700, overflow: 'hidden' }}>
                     <Title>History of events</Title>
-                    <HistoryTable />
+                    <CustomTable rows={rows} columns={columns} showPagination={true} />
                 </Paper>
             </Grid>
         </div>
--- Java API/TRA Project/TRA-Project/src/main/java/dk/diku/TRA/Project/Controller/HistoryController.java
+++ Java API/TRA Project/TRA-Project/src/main/java/dk/diku/TRA/Project/Controller/HistoryController.java
@@ -3,6 +3,7 @@ package dk.diku.TRA.Project.Controller;
 import dk.diku.TRA.Project.Classes.Event;
 import dk.diku.TRA.Project.Services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,13 @@ public class HistoryController {
     @Autowired
     EventService eventService;
 
+    @CrossOrigin
     @GetMapping(path="/getAllEvents")
     public List<Event> GetAllEvents(){
         return eventService.FindAll();
     }
 
+    @CrossOrigin
     @GetMapping(path="/getEventsById")
     public List<Event> GetEventsById(String id){
         return eventService.FindAllByAgentId(id);
