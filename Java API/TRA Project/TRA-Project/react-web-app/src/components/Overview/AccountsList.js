2d05feeb8f93bf36e140945163ef029e07855e30
Alexander <cqv636@alumni.ku.dk>
2022-05-25 13:21:18 +0200

    minor improvements

--- Java API/TRA Project/TRA-Project/react-web-app/src/components/Overview/AccountsList.js
+++ Java API/TRA Project/TRA-Project/react-web-app/src/components/Overview/AccountsList.js
@@ -8,31 +8,34 @@ export const AccountList = () => {
     const[userData, setUserData] = useState({});
 
     useEffect(() => {
-        fetchAccountInfo()
-    }, [])
-
-    const fetchAccountInfo = () => {
         let id = localStorage.getItem('user');
         axios.get(`http://localhost:8080/overview/getAgent/${id}`)
             .then((response) => {
-                setUserData(response.data)
+                    setUserData(response.data)
                 }
             )
             .catch((error) => {
                 console.log(error);
             })
-    }
+    }, [])
+
     return (
         <div>
             <Title>Account info</Title>
             <Typography component="p" variant="h4">
                 Hello {userData.name}!
             </Typography>
+            <p></p>
             <Typography color="text.secondary" sx={{ flex: 1 }}>
                 Account ID: {localStorage.getItem('user')}
             </Typography>
+            <p></p>
             <Typography color="text.secondary" sx={{ flex: 1 }}>
                 Email: {userData.email}
+            </Typography>
+            <p></p>
+            <Typography color="text.secondary" sx={{ flex: 1 }}>
+                Role: {localStorage.getItem('role') == 'admin' ? "Admin" : "Normal user"}
             </Typography>
         </div>
 
